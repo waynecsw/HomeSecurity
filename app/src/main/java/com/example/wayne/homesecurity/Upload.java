92f0b531fe14f4a5c08add1b4d8f47581dabcdda
Jin Hui <jinhui96@hotmail.com>
2018-01-03 01:56:42 +0800

    commit 2

---
+++ app/src/main/java/com/example/wayne/homesecurity/Upload.java
@@ -0,0 +1,33 @@
+package com.example.wayne.homesecurity;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+
+@IgnoreExtraProperties
+public class Upload{
+
+    public String name;
+    public String url;
+
+    // Default constructor required for calls to
+    // DataSnapshot.getValue(User.class)
+    public Upload() {
+    }
+
+    public Upload(String url) {
+        this.url= url;
+    }
+
+    public Upload(String name, String url) {
+        this.name = name;
+        this.url= url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
