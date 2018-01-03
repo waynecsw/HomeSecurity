9b907a553bacb4d140ba842e211e8027f0d43924
Jin Hui <jinhui96@hotmail.com>
2018-01-03 09:30:56 +0800

    commit 3 reverse order

--- app/src/main/java/com/example/wayne/homesecurity/ViewImageActivity.java
+++ app/src/main/java/com/example/wayne/homesecurity/ViewImageActivity.java
@@ -15,6 +15,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ViewImageActivity extends AppCompatActivity {
@@ -49,6 +50,7 @@ public class ViewImageActivity extends AppCompatActivity {
                     Upload upload = new Upload(postSnapshot.getKey(), postSnapshot.getValue().toString());
                     uploads.add(upload);
                 }
+                Collections.reverse(uploads);
                 adapter = new MyAdapter(getApplicationContext(), uploads);
                 recyclerView.setAdapter(adapter);
 
